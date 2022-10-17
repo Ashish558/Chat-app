069cae478556f273dbdd48fe00665be67710b8a4
Ashish558 <ashishprasad2001ap@gmail.com>
2022-10-17 16:42:23 +0530

    added socket

--- app.js
+++ app.js
@@ -50,6 +50,71 @@ app.use('/api/chatroom', chatRoomRoute)
 var messageRoute = require('./routes/message')
 app.use('/api/message', messageRoute)
 
+
+
+// SOCKET 
+const io = require("socket.io")(4100, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+})
+
+let users = []
+
+const addUser = (userId, socketId) => {
+  if (!userId && !socketId) return
+  if (users.find((user) => user.userId === userId)) return
+  users.push({ userId, socketId })
+  console.log('users len :' + users.length)
+}
+
+const removeUser = (socketId) => {
+  users.filter(user => user.socketId !== socketId)
+}
+
+const getUser = (userId) => {
+  return users.find(user => user.userId === userId)
+}
+
+io.on("connection", (socket) => {
+  //when connect
+  console.log("a user connected " + socket.id)
+
+  //take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  })
+
+  //join a room
+  socket.on("joinRoom", ({ roomId, roomName }) => {
+    socket.join(roomId);
+  })
+
+  //send and get message
+  socket.on("sendMessage", ({ sender, chatRoomId, text }) => {
+    const user = getUser(sender._id)
+    // console.log(sender, text)
+    if (!user) return
+    io.in(chatRoomId).emit("getMessage", {
+      sender,
+      chatRoomId,
+      text
+    });
+  })
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+
+});
+
+console.log('socket runnin')
+
+
 const port = process.env.PORT || 4000
 server.listen(port)
 console.log('done')
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
 import AppRoutes from './navigation/RouterConfig.js';
+import { updateIsLoggedIn } from './redux/slices/user.js';
 import './style.css';
 
 function App() {
 
+  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const { token } = JSON.parse(localStorage.getItem("user"))
+    if (token) {
+      dispatch(updateIsLoggedIn(true))
+    }
+  }, [])
+
+  if(loading) return <></>
   return (
     <AppRoutes />
   )
